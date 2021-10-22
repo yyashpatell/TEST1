cd0d3cd28b726302e6bfd0db5ce2feae4a1c5e47
Yash Patel <ypatel6546@conestogac.on.ca>
2021-10-22 10:07:29 -0400

    init

---
+++ Yash_Patel_Test1_Q1.py
@@ -0,0 +1,6 @@
+string = input("enter any alphabetical letter:")
+
+print(string.swapcase())
+
+
+print("output:", string )
---
+++ Yash_Patel_Test1_Q2.py
@@ -0,0 +1,4 @@
+i = ['yash jayeshkumar patel','P','yash patel 8736546']
+
+for f in i:
+    print(f,len(f))
